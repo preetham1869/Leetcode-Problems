072b23daa282e5601e2e6a1e0af31e5dd1d3a4c8
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2025-07-13 04:20:28 +0530

    Time: 4 ms (100%), Space: 44.5 MB (18.07%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev = 0;
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        while (x>rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return x==rev||x==rev/10;
+    }
+}
