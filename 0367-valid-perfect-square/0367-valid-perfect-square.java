da19cd4379195ac90a4e2831c692f3ccea268b68
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2025-07-13 17:16:23 +0530

    Time: 2 ms (14.43%), Space: 40.4 MB (52.2%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1)return true;
+        for(long i=2;i*i<=num;i++){
+            if(i*i==num)return true;
+         }
+        return false;
+    }
+}
