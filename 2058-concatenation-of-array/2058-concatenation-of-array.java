0a60ea51aac4c3355e35ebc5587d432e9736b9d5
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2025-08-27 20:14:52 +0530

    Time: 1 ms (96.11%), Space: 46 MB (8.91%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -1,8 +1,11 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int[] ans = new int[2*nums.length];
-        System.arraycopy(nums, 0, ans, 0, nums.length);
-        System.arraycopy(nums, 0, ans , nums.length,nums.length);
+        int n = nums.length;
+        int[] ans = new int[2*n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
         return ans;
     }
 }
