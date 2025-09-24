5305e87d8a163dcf00ed17d2883ec3f60bf55e16
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2025-09-24 18:32:49 +0530

    Time: 0 ms (100%), Space: 45.3 MB (72.2%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        int miss=n*(n+1)/2;
+        int total=n*(n+1)/2;
         int sum=0;
         for(int num:nums)sum+=num;
-        return miss-sum;
+        return total-sum;
     }
 }
