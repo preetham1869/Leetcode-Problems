2bc1a99baa2d168f933039a4e6fa14223efbf728
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2026-01-06 17:11:45 +0530

    Time: 9 ms (64.42%), Space: 49.3 MB (38.96%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -17,15 +17,15 @@ class Solution {
     public int maxLevelSum(TreeNode root) {
        Queue<TreeNode> q = new LinkedList<>();
        int maxSL=1,maxS=root.val,currL=1;
-       q.add(root);
+       q.offer(root);
        while(!q.isEmpty()){
         int l=q.size();
         int curr=0;
         while(l-->0){
-            TreeNode node =q.remove();
+            TreeNode node =q.poll();
             curr+=node.val;
-            if(node.left!=null)q.add(node.left);
-            if(node.right!=null)q.add(node.right);
+            if(node.left!=null)q.offer(node.left);
+            if(node.right!=null)q.offer(node.right);
         }
         if(maxS<curr){
             maxS=curr;
