fec5ac0c684531ed4877f2b0091bb1ec5c9b4dd9
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2026-01-08 20:48:53 +0530

    Time: 10 ms (85.52%), Space: 139.7 MB (62.92%) - LeetHub

--- 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.java
+++ 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.java
@@ -1,23 +1,11 @@
 class Solution {
     public int minOperations(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        int dup = 0;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
-            if (freq.get(nums[i]) == 2) dup++;
+        HashSet<Integer> set = new HashSet<>();
+        int i = nums.length - 1;
+        while (i >= 0 && !set.contains(nums[i])) {
+            set.add(nums[i]);
+            i--;
         }
-        if (dup == 0) return 0;
-        int ops = 0, i = 0;
-        while (i < nums.length) {
-            for (int k = 0; k < 3 && i < nums.length; k++) {
-                int v = nums[i];
-                freq.put(v, freq.get(v) - 1);
-                if (freq.get(v) == 1) dup--;
-                i++;
-            }
-            ops++;
-            if (dup == 0) return ops;
-        }
-        return ops;
+        return (i + 3) / 3;
     }
 }
