1a6a8ddba94079448a330166b7b29963abf0f8e9
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2026-02-14 09:58:24 +0530

    Time: 1 ms (99.64%), Space: 81 MB (5.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-      int n = nums.length;
-        int sum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++) {
-            if (sum > 0)sum += nums[i];
-            else sum = nums[i];
-            if (sum > maxSum)maxSum = sum;
+        int n=nums.length;
+        int sum=0;
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+           if(sum>0)sum+=nums[i];
+           else sum=nums[i];
+           if(sum>maxi)maxi=sum;
         }
-        return maxSum;  
+        return maxi;
     }
 }
-
-
-//Kadane’s Algorithm finds the **maximum sum of a contiguous subarray** in O(n) by iterating once and updating the current subarray sum or starting a new one.
-//It keeps track of the **best sum so far** and returns it at the end.
