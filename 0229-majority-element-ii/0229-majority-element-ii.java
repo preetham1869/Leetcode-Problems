50a7a3cec5336364c158d5c87980cd530e9148cd
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2026-08-04 14:02:28 +0530

    Time: 2 ms (99.96%), Space: 52.7 MB (29.61%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,33 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> res = new ArrayList<>();
-        int n = nums.length;
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > n / 3) {
-                res.add(entry.getKey());
+        int canditate1=0,canditate2=0;
+        int cnt1=0,cnt2=0;
+        int n=nums.length;
+        for(int num:nums){
+            if(num==canditate1)cnt1++;
+            else if(num==canditate2)cnt2++;
+            else if(cnt1==0){
+                canditate1=num;
+                cnt1=1;
+            }
+            else if(cnt2==0){
+                canditate2=num;
+                cnt2=1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-        return res;
+        cnt1=0;
+        cnt2=0;
+        for(int num:nums){
+            if(num==canditate1)cnt1++;
+            else if(num==canditate2)cnt2++;
+        }
+        List<Integer> ans=new ArrayList<>();
+        if(cnt1>n/3)ans.add(canditate1);
+        if(cnt2>n/3)ans.add(canditate2);
+        return ans;
     }
 }
