bc1e6295cd1ae1021a880574f70eb2c40daf9f82
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2026-08-09 16:58:20 +0530

    Time: 20 ms (83.42%), Space: 57 MB (5.25%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0;
+        int j = people.length - 1;
+        int cnt = 0;
+        while (i <= j) {
+            if (people[i] + people[j] <= limit)i++;
+            j--;
+            cnt++;
+        }
+        return cnt;
+    }
+}
