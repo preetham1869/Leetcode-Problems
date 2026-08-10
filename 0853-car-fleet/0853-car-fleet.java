fe54c23f922b936c83a0191dc6c44c4f2bb3cc80
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2026-08-10 15:16:23 +0530

    Time: 98 ms (18.38%), Space: 99.8 MB (39.38%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n=position.length;
+        double cars[][]=new double[n][2];
+        for(int i=0;i<n;i++){
+            cars[i][0]=position[i];
+            cars[i][1]=(double)(target-position[i])/speed[i];
+        }
+        Arrays.sort(cars,(a,b)->Double.compare(b[0],a[0]));
+        Stack<Double> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            double time=cars[i][1];
+            if(st.isEmpty() || time>st.peek()){
+                st.push(time);
+            }
+        }
+        return st.size();
+    }
+}
