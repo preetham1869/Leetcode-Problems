acc36ac43395e9d332a90cc059c0c703dceb4d96
Preetham Velamala <23mh1a05l1@acoe.edu.in>
2026-08-22 11:44:18 +0530

    Time: 5 ms (21.79%), Space: 41.2 MB (99.93%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int n = nums.length;
+        int maxi= 0;
+        for (int i= 0;i<(1 << n);i++) {
+            int xor = 0;
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    xor ^= nums[j];
+                }
+            }
+            maxi+=xor;
+        }
+        return maxi;     
+    }
+}
